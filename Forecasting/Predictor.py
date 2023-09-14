64373a3e52a79ef119129ca9c409e3582ca1b7c9
Dongz12345 <wjdehdwn7884@konkuk.ac.kr>
2023-09-14 17:14:06 +0900

    test

---
+++ Forecasting/Predictor.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import os
+
+def TargetDataReader(Loc, FileName):
+    Path = os.path.join(Loc, FileName)
+    TempWeather = pd.read_csv(Path)
+    TempWeather["DeliveryDT"] = pd.to_datetime(TempWeather["DeliveryDT"],
+                                             format='%Y-%m-%d %H:%M:%S',
+                                             utc=False)
+
+    return TempWeather
